9c22a83fc011f98a40d926bd29bd8d345155e700
Peter Jang <peter@Peters-MacBook-Air.local>
2023-07-23 23:27:34 -0400

    added a basic prompt

---
+++ app/src/main/java/com/example/tester/Prompt.java
@@ -0,0 +1,23 @@
+package com.example.tester;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+
+public class Prompt extends AppCompatDialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Prompt")
+                .setMessage("Please select task")
+                .setPositiveButton("Select", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+        return builder.create();
+    }
+}
