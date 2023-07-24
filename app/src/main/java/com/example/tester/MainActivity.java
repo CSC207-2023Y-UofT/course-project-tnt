bb9706d22981b18c6fb8bc4ac631169cc827b7bd
mariinakim <marina.kim@mail.utoronto.ca>
2023-07-23 22:46:55 -0400

    created main page

--- app/src/main/java/com/example/tester/MainActivity.java
+++ app/src/main/java/com/example/tester/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.tester;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton signinbtn = (MaterialButton) findViewById(R.id.signinbtn);
+        MaterialButton registerbtn = (MaterialButton) findViewById(R.id.registerbtn);
+
+        signinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Open UserRegUI
+            }
+        });
+
     }
 }
