b3fdaccf35667d979815f7c3b1419dbb293f81db
yoyogong <yoyogongtian@gmail.com>
2023-08-09 23:26:21 -0400

    - Updates testers with the updated implementation of timer prompt method - Complete Java Doc - Add context=".PomodoroTimer" in xml for navigation

--- app/src/main/java/com/example/tester/PomodoroTimer.java
+++ app/src/main/java/com/example/tester/PomodoroTimer.java
@@ -1,8 +1,3 @@
-/**
- * A fragment class that implements the Pomodoro Timer functionality.
- * This class provides a timer for work and break intervals using the Pomodoro Technique.
- */
-
 package com.example.tester;
 
 import android.annotation.SuppressLint;
@@ -17,20 +12,46 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+/**
+ * A fragment class that implements the Pomodoro Timer functionality.
+ * This class provides a timer for work and break intervals using the Pomodoro Technique.
+ */
 public class PomodoroTimer extends Fragment {
+    /** The TextView displaying the timer. */
     public TextView timerTextView;
+
+    /** The button to start or pause the timer. */
     public Button startPauseButton;
+
+    /** The button to reset the timer. */
     public Button resetButton;
+
+    /** The CountDownTimer instance for managing the timer. */
     public CountDownTimer timer;
+
+    /** Indicates whether the timer is currently running. */
     public boolean isTimerRunning;
+
+    /** The remaining time in milliseconds. */
     public long timeLeftInMillis;
+
+    /** The duration of a work session in milliseconds. */
     public static final long WORK_DURATION = 25 * 60 * 1000;
+
+    /** The duration of a break session in milliseconds. */
     public static final long BREAK_DURATION = 5 * 60 * 1000;
 
-    private boolean showPromptOnBreakFinish = false;
-
+    /** The helper class for handling vibration. */
     public VibratorHelper vibratorHelper;
 
+    /** Indicates if the prompt should be shown */
+    public boolean showPromptOnBreakFinish = false;
+
+    /**
+     * The custom prompt to display when the timer prompts the user.
+     * The prompt provides options to continue working or reset the timer.
+     */
     Prompt TimerPrompt = new CustomPrompt(
             getContext(),
             "Good Job!",
@@ -49,7 +70,8 @@ public class PomodoroTimer extends Fragment {
         }
     };
 
-
+    /**
+     * Initializes the UI elements and sets up button click listeners. */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
@@ -71,7 +93,11 @@ public class PomodoroTimer extends Fragment {
         return rootView;
     }
 
-    private void startTimer(long duration) {
+    /**
+     * Starts the timer with the specified duration.
+     * If the timer finishes, it triggers vibration and optionally shows a prompt.
+     */
+    public void startTimer(long duration) {
         timer = new CountDownTimer(duration, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -101,25 +127,41 @@ public class PomodoroTimer extends Fragment {
         }
     }
 
-    private void pauseTimer() {
+    /**
+     * Pauses the timer and updates the UI accordingly.
+     */
+    public void pauseTimer() {
         timer.cancel();
         isTimerRunning = false;
         startPauseButton.setText("Start");
         resetButton.setVisibility(View.VISIBLE);
     }
-    private void resetTimer() {
+
+    /**
+     * Resets the timer to the default work duration and updates the UI accordingly.
+     */
+    public void resetTimer() {
         timer.cancel();
         timeLeftInMillis = WORK_DURATION;
         updateTimerText(WORK_DURATION);
         startPauseButton.setText("Start");
         resetButton.setVisibility(View.GONE);
     }
-    private void updateTimerText(long millisUntilFinished) {
+
+    /**
+     * Updates the current countdown time and updates the UI accordingly.
+     */
+    public void updateTimerText(long millisUntilFinished) {
         int minutes = (int) (millisUntilFinished / 1000) / 60;
         int seconds = (int) (millisUntilFinished / 1000) % 60;
         @SuppressLint("DefaultLocale") String timeLeft = String.format("%02d:%02d", minutes, seconds);
         timerTextView.setText(timeLeft);
     }
+
+    /**
+     * Handles the onStop event of the fragment.
+     * Cancels the timer to prevent leaks when the fragment is stopped.
+     */
     @Override
     public void onStop() {
         super.onStop();
