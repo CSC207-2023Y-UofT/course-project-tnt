cf169d6a0496de84337d5e5893c7a4e174297401
Zitherlight <sebastian.nguyen@mail.utoronto.ca>
2023-08-10 07:08:22 -0400

    3-task-list backup complete implementation

--- app/src/main/java/com/example/tester/TaskListFragment.java
+++ app/src/main/java/com/example/tester/TaskListFragment.java
@@ -2,9 +2,13 @@ package com.example.tester;
 
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.tester.Adapters.TaskAdapter;
 import com.example.tester.Model.TaskModel;
 import com.example.tester.Utils.DatabaseHandler;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Collections;
@@ -50,6 +55,23 @@ public class TaskListFragment extends AppCompatActivity implements DialogCloseLi
         tasksAdapter.setTasks(taskList);
 
         fab.setOnClickListener(v -> AddTaskUseCase.newInstance().show(getSupportFragmentManager(), AddTaskUseCase.TAG));
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.timer) {
+                replaceFragment(new PomodoroTimer());
+                return true;
+            } else if (itemId == R.id.tasks) {
+                return true;
+            } else if (itemId == R.id.logout) {
+                DatabaseHandler.deleteAllTasks();
+                Intent intent = new Intent(TaskListFragment.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            return false;
+        });
     }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -59,5 +81,15 @@ public class TaskListFragment extends AppCompatActivity implements DialogCloseLi
         Collections.reverse(taskList);
         tasksAdapter.setTasks(taskList);
         tasksAdapter.notifyDataSetChanged();
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
+        // Close the current activity
+        finish();
     }
 }
